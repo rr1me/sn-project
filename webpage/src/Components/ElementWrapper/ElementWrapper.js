cc2b67658c4e432e44b3dfb0bd13e667b8332bcd
rime <s1337228@yandex.ru>
2022-12-16 22:32:56 +0300

    scroll animation

--- webpage/src/Components/ElementWrapper/ElementWrapper.js
+++ webpage/src/Components/ElementWrapper/ElementWrapper.js
@@ -1,12 +1,29 @@
 import './ElementWrapper.sass';
 import Header from "../Header/Header";
+import {useEffect, useRef, useState} from "react";
 
 const ElementWrapper = ({children}) => {
+    const contentRef = useRef();
+    const [height, setHeight] = useState('auto');
+
+    useEffect(() => {
+        if (contentRef.current){
+            const v = contentRef.current.getBoundingClientRect().height;
+            console.log(contentRef.current.getBoundingClientRect());
+            // console.log(v);
+            // console.log(v-(34 + 40));
+            // setHeight(window.innerHeight);
+        }
+    }, []);
+
     return (
         <div className='elementWrapper'>
             <div className='content'>
                 <Header/>
                 {children}
+                {/*<button onClick={() => {*/}
+                {/*    console.log(contentRef.current.getBoundingClientRect())*/}
+                {/*}}>press</button>*/}
             </div>
         </div>
     )
