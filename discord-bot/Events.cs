71bb0cabcfcef4a3aa41ccd906ea3db6b0b10fbf
rime <s1337228@yandex.ru>
2023-03-13 15:19:13 +0300

    log fix

--- discord-bot/Events.cs
+++ discord-bot/Events.cs
@@ -9,7 +9,7 @@ namespace discordBot;
 public class Events
 {
     private readonly IServiceProvider _serviceProvider;
-    
+
     private readonly DiscordSocketClient _client;
     private readonly CommandService _commands;
 
@@ -19,7 +19,8 @@ public class Events
     private readonly ILogger<Events> _logger;
 
 
-    public Events(DiscordSocketClient client, CommandService commands, Miscellaneous miscellaneous, Settings settings, IServiceProvider serviceProvider, ILogger<Events> logger)
+    public Events(DiscordSocketClient client, CommandService commands, Miscellaneous miscellaneous, Settings settings,
+        IServiceProvider serviceProvider, ILogger<Events> logger)
     {
         _client = client;
         _commands = commands;
@@ -32,38 +33,41 @@ public class Events
     public async Task OnMessageReceived(SocketMessage messageParam)
     {
         if (messageParam is not SocketUserMessage message) return;
-        
+
         var argPos = 0;
 
-        if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)) || message.Author.IsBot)
+        if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+            message.Author.IsBot)
             return;
-        
+
         var context = new SocketCommandContext(_client, message);
 
         await _commands.ExecuteAsync(context: context, argPos: argPos, services: _serviceProvider);
     }
-    
+
     public async Task OnReady() => _miscellaneous.CompareSettings();
 
     public async Task OnUserJoined(SocketGuildUser user)
     {
         var _settingsEntity = _settings._settingsEntity;
-            
+
         var channel = _client.GetChannel(_settingsEntity.MentionChannelId) as IMessageChannel;
         var msg = await channel.SendMessageAsync(user.Mention);
-    
+
         msg.DeleteAsync();
     }
-    
-    public Func<Cacheable<IUserMessage, ulong>, Cacheable<IMessageChannel, ulong>, SocketReaction, Task> GetReactionMethod(bool isAdded) 
+
+    public Func<Cacheable<IUserMessage, ulong>, Cacheable<IMessageChannel, ulong>, SocketReaction, Task>
+        GetReactionMethod(bool isAdded)
         => async (_, _, socketReaction) =>
         {
             var _settingsEntity = _settings._settingsEntity;
-            
+
             var emoteAndRole = _settingsEntity.EmoteAndRole;
             var emoteId = socketReaction.Emote.ToString();
 
-            if (socketReaction.MessageId != _settingsEntity.MessageId || socketReaction.User.Value.IsBot || !emoteAndRole.Keys.Contains(emoteId)) return;
+            if (socketReaction.MessageId != _settingsEntity.MessageId || socketReaction.User.Value.IsBot ||
+                !emoteAndRole.Keys.Contains(emoteId)) return;
 
             var user = socketReaction.User.Value as IGuildUser;
             if (isAdded)
@@ -82,12 +86,11 @@ public class Events
                 _logger.LogInformation(msg);
                 return;
             }
+
             _logger.LogWarning(msg);
-            
         }
-        LogMethod($"{log.Source} | {log.Message}");
-        
+
         var exp = log.Exception;
-        if (exp == null) LogMethod(exp.ToString());
+        LogMethod($"{log.Source} | {(exp == null ? log.Message : exp.ToString())}");
     }
 }
